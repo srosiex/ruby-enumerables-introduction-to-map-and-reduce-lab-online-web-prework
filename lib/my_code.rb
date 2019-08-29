51ef16b4b27e1f990fcb9e08c99fa90120a4bc69
Sarah H <sarahrose.henderson@gmail.com>
2019-08-29 03:57:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,13 +49,13 @@ end
 total
 end
 
-def reduce_to_all_true(array)
-  counter = 0
-  while counter < array.length do
-    return true if array[counter] #condition
-     counter += 1
-end
-return false
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i]
+    i += 1
+  end
+  return false
 end
 
 def reduce_to_all_true(source_array)
